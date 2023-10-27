d7de76d9f63712b8353f9ab03c84bf7f54ae8778
Andrea Pistolato <apistolato@tecnostrutture.eu>
2023-10-27 14:35:15 +0200

    test10

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -86,7 +86,7 @@ export const Hero = () => {
         />
         <CollectionInfoBox
           content={`${collectionSize || 0}`}
-          isLoading={collectionSizeLoading || totalTokensLeftIsLoading}
+          isLoading={collectionSizeLoading || 0}
           label="APY"
         />
       </Box>
