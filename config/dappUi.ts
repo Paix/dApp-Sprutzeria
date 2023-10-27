d587de8bdee7207589679f30aa04bfff0b7a0609
Andrea Pistolato <apistolato@tecnostrutture.eu>
2023-10-27 14:14:12 +0200

    test8

--- config/dappUi.ts
+++ config/dappUi.ts
@@ -31,11 +31,6 @@ export const faq = [
     answer:
       'It is crucial because these two prove that the NFTs come from a verified source. The NFT project should always show the collection ticker and minter smart contract to gain trust.',
   },
-  {
-    question: 'What is Elven Tools?',
-    answer:
-      'The Elven Tools is an open-source toolset including the CLI tool, Smart Contract for handling the NFT collections, and this dapp template. You can, of course, use each tool separately. But the CLI helps with smart contract deployments and the setup process. You can also interact with the smart contract using the CLI or even use it as a buyer.',
-  },
 ];
 
 // Roadmap section data
