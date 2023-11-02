f63a6d237a0144550535593fe9f9df0a84dc3137
Andrea Pistolato <apistolato@tecnostrutture.eu>
2023-11-02 12:43:15 +0100

    Test

--- pages/staking.tsx
+++ pages/staking.tsx
@@ -3,8 +3,6 @@ import { Box } from '@chakra-ui/react';
 import { MainLayout } from '../components/MainLayout';
 import { HeaderMenu } from '../components/HeaderMenu';
 import { HeaderMenuButtons } from '../components/HeaderMenuButtons';
-import { MintHero } from '../components/MintHero';
-import { HeroImage } from '../components/HeroImage';
 
 const Staking: NextPage = () => {
   return (
